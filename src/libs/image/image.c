be77003d4532dfc7a7e96acf11fc7679be0e5502
Xie Yuheng <xyheme@gmail.com>
2024-12-02 18:08:48 +0800

    up

--- src/libs/image/image.c
+++ src/libs/image/image.c
@@ -1,1 +1,13 @@
 #include "index.h"
+
+// uint8_t parse_image_width(const char *path) {
+//     size_t length = strlen(path);
+//     const char *width_start = path + (length - 9);
+//     return strtol(width_start, NULL, 16);
+// }
+
+// uint8_t parse_image_height(const char *path) {
+//     size_t length = strlen(path);
+//     const char *height_start = path + (length - 6);
+//     return strtol(height_start, NULL, 16);
+// }
--- src/libs/image/image.h
+++ src/libs/image/image.h
@@ -1,1 +1,4 @@
 #pragma once
+
+// uint8_t parse_image_width(const char *path);
+// uint8_t parse_image_height(const char *path);
