accd3c7938c6fa3e402d59d713327c0d9e481ac6
Xie Yuheng <xyheme@gmail.com>
2024-12-04 21:12:19 +0800

    [canvas] `canvas_clickable_area_new`

--- src/libs/canvas/canvas_clickable_area.c
+++ src/libs/canvas/canvas_clickable_area.c
@@ -1,1 +1,15 @@
 #include "index.h"
+
+canvas_clickable_area_t *canvas_clickable_area_new(
+    size_t x, size_t y,
+    size_t width, size_t height,
+    on_click_t *on_click
+) {
+    canvas_clickable_area_t *self = allocate(sizeof(canvas_clickable_area_t));
+    self->x = x;
+    self->y = y;
+    self->width = width;
+    self->height = height;
+    self->on_click = on_click;
+    return self;
+}
--- src/libs/canvas/canvas_clickable_area.h
+++ src/libs/canvas/canvas_clickable_area.h
@@ -5,3 +5,8 @@ struct canvas_clickable_area_t {
     size_t width, height;
     on_click_t *on_click;
 };
+
+canvas_clickable_area_t *canvas_clickable_area_new(
+    size_t x, size_t y,
+    size_t width, size_t height,
+    on_click_t *on_click);
