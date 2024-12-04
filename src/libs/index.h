e682be57b0d37b2ecbc5bd66d5debdb8f055494f
Xie Yuheng <xyheme@gmail.com>
2024-12-05 00:00:02 +0800

    extract `libs/index.h`

---
+++ src/libs/index.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "allocate/index.h"
+#include "string/index.h"
+#include "list/index.h"
+#include "dict/index.h"
+#include "file/index.h"
+#include "stack/index.h"
+#include "int/index.h"
+#include "text/index.h"
+#include "file/index.h"
+#include "image/index.h"
+#include "canvas/index.h"
+#include "commander/index.h"
