f9bebcbce3a1a3a75a9bb726c6d7becd05dc6142
Xie Yuheng <xyheme@gmail.com>
2024-12-09 02:00:51 +0800

    `text_from_string` should call `utf8_iter_destroy(&iter)`

--- src/libs/font/font.c
+++ src/libs/font/font.c
@@ -24,7 +24,21 @@ font_destroy(font_t **self_pointer) {
     }
 }
 
-// font_t *
-// font_load_hex_file(file_t *file) {
+font_t *
+font_load_hex_file(file_t *file) {
+    font_t *font = font_new();
+    char *string = file_read_string(file);
 
-// }
+    // TODO
+
+    // line_iter_t *iter = line_iter_new(string);
+    // char *line = line_iter_start(iter);
+    // while (line) {
+    //     glyph_t *glyph = glyph_parse_hex(line);
+    //     glyph
+    //     line = line_iter_next(iter);
+    // }
+
+    free(string);
+    return font;
+}
