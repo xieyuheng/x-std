d71b1053f7b0dc2500002d88522e9252c714ecc9
Xie Yuheng <xyheme@gmail.com>
2024-12-09 04:29:14 +0800

    `font_test` quit printing too much

--- src/libs/font/font_test.c
+++ src/libs/font/font_test.c
@@ -16,15 +16,15 @@ font_test(void) {
     file_t *file = file_open_or_fail(file_name, "r");
     font_t *font = font_from_hex_file(file);
 
-    glyph_t *glyph = font_first_glyph(font);
-    while (glyph) {
-        printf("ascii code point: ox%x\n", glyph_code_point(glyph));
-        printf("\n");
-        glyph_print_ascii_art(glyph, ' ', '#');
-        printf("\n");
+    // glyph_t *glyph = font_first_glyph(font);
+    // while (glyph) {
+    //     printf("ascii code point: ox%x\n", glyph_code_point(glyph));
+    //     printf("\n");
+    //     glyph_print_ascii_art(glyph, ' ', '#');
+    //     printf("\n");
 
-        glyph = font_next_glyph(font, glyph_code_point(glyph));
-    }
+    //     glyph = font_next_glyph(font, glyph_code_point(glyph));
+    // }
 
     font_destroy(&font);
 
